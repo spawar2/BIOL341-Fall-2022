fdc45ec8f9b8910bd0c972996e5c0cb5532111f9
Pawar <pawar1550@gmail.com>
2022-10-28 10:32:41 -0400

    Add files via upload

--- Oct-26.R
+++ Oct-26.R
@@ -10,4 +10,9 @@ library(geiger)
 # Read and write csv file
 setwd("/Users/pawar/Desktop")
 
+# To read only the csv files
 data <- read.csv("Book1.csv", header=FALSE)
+
+# To read only text files formats rtf or txt
+
+data <- read.delim("Book1.rtf", header=FALSE)
