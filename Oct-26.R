823fea005ba0517c5cefe3d25cf4f37d954bcfe0
Pawar <pawar1550@gmail.com>
2022-10-31 12:58:44 -0400

    Add files via upload

--- Oct-26.R
+++ Oct-26.R
@@ -16,3 +16,23 @@ data <- read.csv("Book1.csv", header=FALSE)
 # To read only text files formats rtf or txt
 
 data <- read.delim("Book1.rtf", header=FALSE)
+
+# Write the data in a csv file
+
+list <- c(1,2,3,4,5,6)
+
+write.csv(list, "new_file.csv")
+
+write.table(list, "new_file.txt")
+
+
+
+
+
+
+
+
+
+
+
+
